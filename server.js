463b8a509ca0a6634ad68af79bccf2997a150654
supermelody90 <supermelody90@gmail.com>
2017-08-14 12:37:41 +1000

    trying to solve ajax's recieving problem

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 var express = require('express');
 var path = require('path');
-// var multer = require('multer');
 var fs = require('fs');
+var url = require('url');
 var bodyParser = require('body-parser');
 var app = express();
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-// app.use(multer);
 
-app.listen(1185, function() {
-    console.log('server listening on port 1185');
+app.listen(8080, function() {
+    // var reqPath = url.parse(req.url).pathname;
+    console.log('server listening on port 8080');
 });
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
-    // res.write('hello');
 });
+
+app.get('/', function(req, res) {
+    console.log("request recieved");
+    var fileData = fs.readFileSync('data.json', 'utf-8');
+    res.writeHead(200, {"Content-Type": "text/plain","Access-Control-Allow-Origin":"http://localhost"});
+    res.send(fileData);
+    console.log("data sent");
+})
 
 app.post('/', function(req, res) {
     // console.log(req.body);
     var jsonData = req.body;
+    req.body.title.set
     delete jsonData.submit;
     var jsonString = JSON.stringify(jsonData);
     fs.writeFile('data.json', jsonString, function(err) {
@@ -31,3 +40,10 @@ app.post('/', function(req, res) {
     console.log(jsonData);
     res.end('Edit Success!');
 })
+
+// module.exports = function(imitator) {
+//     imitator({
+//         url: '/',
+//         result: JSON.parse(fs.readFileSync('data.json', 'utf-8'))
+//     });
+// }
